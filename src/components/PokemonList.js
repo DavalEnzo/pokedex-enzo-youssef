aa6ff902c40a3d1e0598a7a3134d141b3bae9492
unknown <elgrysyoussef78@gmail.com>
2023-05-23 16:50:32 +0200

    add icon, image, data

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import pokemonData from './pokemons.json';
+import css from '../css/style.css';
 
 function PokemonList() {
   return (
-    <div>
+    <div className='container'>
       <h1>Pokédex - Première génération</h1>
+
       <ul>
+      <div className='container-box'>
         {pokemonData.map((pokemon) => (
-          <li key={pokemon.id}>
+          <li className='list btn' key={pokemon.id}>
+           
             <Link to={`/pokemon/${pokemon.id}`}>
-              {pokemon.name} - {pokemon.type}
+            <img src={pokemon.image} alt={pokemon.name} />
+              {pokemon.name} - {pokemon.apiTypes[0].name}
             </Link>
           </li>
         ))}
+        </div>
       </ul>
+      
     </div>
   );
 }
